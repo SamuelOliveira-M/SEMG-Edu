e98ec94e3ec1e65b7dd36679369d2e5c484eb5d2
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-04-09 15:26:45 -0300

    refactor: Changing the method return ReadCalendarModel

--- src/useCase/calendario/read/ReadCalendarModel.ts
+++ src/useCase/calendario/read/ReadCalendarModel.ts
@@ -4,7 +4,36 @@ import { prisma } from "../../../lib/prisma"
 class ReadCalendarModel{
 	async readCalendarModel(){
 
-		const calendar = await prisma.calendario.findMany({})
+		const calendar = await prisma.calendario.findMany({
+			select:{
+				diaSemana:true,
+				horario:{
+					select:{
+						horarioInicio:true,
+						horarioFim:true
+					}
+				},
+				lotacao:{
+					select:{
+						professor:{
+							select:{
+								nome:true
+							}
+						},
+						turma:{
+							select:{
+								nome:true
+							}
+						},
+						disciplina:{
+							select:{
+								nome:true
+							}
+						}
+					}
+				}
+			},					
+		})
 		return calendar
 	}
 }
