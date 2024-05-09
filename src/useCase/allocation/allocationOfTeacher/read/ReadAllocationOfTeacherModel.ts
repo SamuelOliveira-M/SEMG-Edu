d504ecb67c8e8f6017a1230119b654f656f0b8b9
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-05-09 17:42:13 -0300

    feat: method of checking whether teachers are already staffed

--- src/useCase/allocation/allocationOfTeacher/read/ReadAllocationOfTeacherModel.ts
+++ src/useCase/allocation/allocationOfTeacher/read/ReadAllocationOfTeacherModel.ts
@@ -4,7 +4,7 @@ import { prisma } from "../../../../lib/prisma"
 class ReadAllocationOfTeacherController{
 	
 	async AllocationOfTeacherModel(schoolClassId:string){
-		const AllocationOfTeacher = await prisma.professor_Disciplina_Turma.findMany({
+		const AllocationOfTeacher = await prisma.lotacao.findMany({
 			where: {
 				turmaId: schoolClassId
 			},
@@ -29,41 +29,62 @@ class ReadAllocationOfTeacherController{
 					}
 				}
 			}
-
-			
-			
-			// where:{
-			// 	id:schoolClassId
-			// },
-			// select:{
-			// 	disciplinasTurmas:{
-			// 		select:{
-			// 			professor:{
-			// 				select:{
-			// 					id: true,
-			// 					nome: true,
-      // 					email:true,
-      // 					url_image:true,
-
-			// 					disciplinasTurmas:{
-			// 						select:{
-			// 							disciplina:{
-			// 								select:{
-			// 									id:true,
-			// 									nome:true
-			// 								}
-			// 							}
-			// 						}
-			// 					}
-			// 				}
-			// 			}
-			// 		}
-			// 	}
-			// }
 		})
 					
 		return AllocationOfTeacher
 	}
+
+	async lotacaoAlreadExist(
+		className:string,
+		teacherName:string,
+		subjectName:string
+	){
+
+		const classAlereadExist = await prisma.turma.findUnique({
+			where:{
+				nome:className
+			}
+		})
+		if(!classAlereadExist){
+			return "Turma não existe"
+		}
+
+		const teacherAlereadExist = await prisma.professor.findUnique({
+			where:{
+				nome:teacherName
+			}
+		})
+		if(!teacherAlereadExist){
+			return "Turma Professor não existe"
+		}
+
+		const subjectAlereadExist = await prisma.disciplina.findUnique({
+			where:{
+				nome:subjectName
+			}
+		})
+		if(!subjectAlereadExist){
+			return "Turma Professor não existe"
+		}
+
+
+
+		const lotacao = await prisma.lotacao.findFirst({
+			where:{
+				disciplinaId:subjectAlereadExist.id,
+				professorId:teacherAlereadExist.id,
+				turmaId:classAlereadExist.id
+			}
+		})
+
+		if(!lotacao){
+			return 'lotação não existe'
+		}
+
+		return lotacao
+
+	}
+
 }
 
 export default new ReadAllocationOfTeacherController
