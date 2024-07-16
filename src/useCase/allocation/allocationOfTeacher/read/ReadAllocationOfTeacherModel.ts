a8e1efb4d5f4215753dc95e8543067fcc7f76874
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-07-16 16:27:36 -0300

    refactor: modifição da condição da consulta do prisma

--- src/useCase/allocation/allocationOfTeacher/read/ReadAllocationOfTeacherModel.ts
+++ src/useCase/allocation/allocationOfTeacher/read/ReadAllocationOfTeacherModel.ts
@@ -35,45 +35,12 @@ class ReadAllocationOfTeacherController{
 	}
 
 	async lotacaoAlreadExist(
-		className:string,
-		teacherName:string,
-		subjectName:string
+		allocationId:string
 	){
 
-		const classAlereadExist = await prisma.turma.findUnique({
+		const lotacao = await prisma.lotacao.findUnique({
 			where:{
-				nome:className
-			}
-		})
-		if(!classAlereadExist){
-			return "Turma não existe"
-		}
-
-		const teacherAlereadExist = await prisma.professor.findUnique({
-			where:{
-				nome:teacherName
-			}
-		})
-		if(!teacherAlereadExist){
-			return "Turma Professor não existe"
-		}
-
-		const subjectAlereadExist = await prisma.disciplina.findUnique({
-			where:{
-				nome:subjectName
-			}
-		})
-		if(!subjectAlereadExist){
-			return "Turma Professor não existe"
-		}
-
-
-
-		const lotacao = await prisma.lotacao.findFirst({
-			where:{
-				disciplinaId:subjectAlereadExist.id,
-				professorId:teacherAlereadExist.id,
-				turmaId:classAlereadExist.id
+				id:allocationId
 			}
 		})
 
