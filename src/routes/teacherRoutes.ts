808b5a2d16c216169bac30898c04b1da1e0d603a
samueloliveira_m <trabalho.samueloliv@gmail.com>
2024-07-30 13:35:25 -0300

    refactor: add new routes teacher

--- src/routes/teacherRoutes.ts
+++ src/routes/teacherRoutes.ts
@@ -2,12 +2,14 @@ import express  from "express";
 
 import ReadSubjectController from "../useCase/disciplina/read/ReadSubjectController";
 import DeleteTeacherController from "../useCase/professor/delete/DeleteTeacherController";
-
+import ReadTeacherController from "../useCase/professor/read/ReadTeacherController";
 
 const teacherRoutes = express.Router();
 
 teacherRoutes.get('/subjects/:turmaId/:professorId',ReadSubjectController.classTeacherSubjects);
 teacherRoutes.get('/redimentoss/:turmaId/:disciplinaId',ReadSubjectController.studentPerformanceSheet)
+teacherRoutes.get("/teacherstt",ReadTeacherController.readAllTeachersController)
+teacherRoutes.get("/teacherProfile/:id",ReadTeacherController.readTeachersClasses)
 
 teacherRoutes.delete("/remove/teacher/:id",DeleteTeacherController.deleteTeacherController)
 
